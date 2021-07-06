05fd6d03999e9233db28f9459aea30af67c76bad
iamvpa <vpacorp@hotmail.com>
2021-07-06 21:50:29 +0530

    added rankk

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,8 +1,20 @@
 <template>
-    <p>Reaction Time - {{score}}</p>
+<div> <p>Reaction Time - {{score}}</p>
+    <p>{{rank}}</p></div>
+   
 </template>
 <script>
 export default {
-  props: ["score"]
+  props: ["score"],
+  data() {
+    return {
+      rank: null
+    };
+  },
+  mounted() {
+    if (this.score < 250) this.rank = "Ninja Fingers";
+    else if (this.score < 400) this.rank = "Rapid Reflexes";
+    else this.rank = "Snail Pace";
+  }
 };
 </script>

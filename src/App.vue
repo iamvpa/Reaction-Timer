25c62ed7e6f35d7c33865b6353ee9dd432a7c0b9
iamvpa <vpacorp@hotmail.com>
2021-07-06 21:39:11 +0530

    custom events

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+<h1>Ninja Reaction timer</h1>
+  <button @click="start" :disabled="isPlaying" >play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <p v-if="showResults">Reaction time:{{score}} ms</p>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Block";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { Block },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      console.log(this.delay);
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.showResults = true;
+      this.score = reactionTime;
+      this.isPlaying = false;
+      console.log(this.score);
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -20,7 +45,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
